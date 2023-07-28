78b080e0d60147600cfeb36a4d198f942fd2b34f
Maria Meir <pigyarx@gmail.com>
2023-07-28 09:22:59 -0400

    restante das resoluções

--- 02-o-que-e-doom-exercicio/script.js
+++ 02-o-que-e-doom-exercicio/script.js
@@ -1,12 +1,15 @@
 // Retorne o url da página atual utilizando o objeto window
 const href = window.location.href;
-
 console.log(href);
-
 
 // Seleciona o primeiro elemento da página que
 // possua a classe ativo
+const titulo = document.querySelector('h1');
+console.log(titulo.classList);
 
 // Retorne a linguagem do navegador
+const lingua = window.navigator.language;
+console.log(lingua);
 
 // Retorne a largura da janela 
+const largura = window.innerWidth;
