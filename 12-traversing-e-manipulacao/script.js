292d64e59f96aa47f4197dc6300d38ef0dc5700d
Maria Meir <pigyarx@gmail.com>
2023-08-08 14:27:24 -0400

    Traversing e Manipulação

---
+++ 12-traversing-e-manipulacao/script.js
@@ -0,0 +1,2 @@
+// OUTERHTML, INNERHTML E INNERTEXT
+// Propriedades que retornam uma string contendo o html ou texto. É possível atribuir um novo valor para as mesmas element.innerText = 'Novo Texto'.
